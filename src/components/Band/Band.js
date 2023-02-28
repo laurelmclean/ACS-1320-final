dfd03745411f89f59440fb6e2b3cce6c9f4afc90
Laurel McLean <laurel.mclean@live.ca>
2023-02-28 12:56:27 -0700

    stretch challenges

--- src/components/Band/Band.js
+++ src/components/Band/Band.js
@@ -3,6 +3,9 @@ import './Band.css';
 
 function Band(props) {
     const { band_name, formed, origin, fans, split, style } = props
+    const styles = style.split(',');
+    const styleItems = styles.map(style => <li key={style}>{style}</li>);
+
     return (
         <div>
             <h1>{band_name}</h1>
@@ -10,7 +13,8 @@ function Band(props) {
             <p><strong>Fans:</strong> {(fans * 1000).toLocaleString('en')}</p>
             <p><strong>Formed:</strong> {formed}</p>
             <p><strong>Split:</strong> {split}</p>
-            {style}
+            <p><strong>Style:</strong></p>
+            <ul>{styleItems}</ul>
         </div>
     )
 }
--- src/components/MetalMeta/MetalMeta.js
+++ src/components/MetalMeta/MetalMeta.js
@@ -3,10 +3,17 @@ import React from 'react';
 import './MetalMeta.css'
 
 function MetalMeta() {
+    let totalFans = 0;
+    for (let i = 0; i < data.length; i++) {
+        totalFans += data[i].fans;
+    }
+    const formattedTotalFans = (totalFans * 1000).toLocaleString();
+
     return (
         <div className='Metal-meta'>
             <h2>Metal Bands 🤘</h2>
             <h2>Bands: {data.length}</h2>
+            <h2>Total Fans: {formattedTotalFans} </h2>
         </div>
     )
 }
